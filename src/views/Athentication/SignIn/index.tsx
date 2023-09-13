78492a5ba6e8445514da628d25125dca028d82b1
PyoChangYeong <game1425@naver.com>
2023-09-13 18:03:59 +0900

    09.13 login 토큰 전달 성공

---
+++ src/views/Athentication/SignIn/index.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import { useCookies } from 'react-cookie';
+
+// mui.com
+import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
+import CardContent from '@mui/material/CardContent';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import CardActions from '@mui/material/CardActions';
+import axios from 'axios';
+
+
+export default function SignIn() {
+    const [email,setEmail] =useState('')
+    const [password,setPassword] =useState('')
+    const [cookie,setCookies] = useCookies();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    
+
+
+    
+    const signInhandler =() =>{
+        if(email.length === 0 || password.length === 0){
+        alert('이메일과 비밀번호를 입력하세요')
+        return;
+    }
+
+    
+    const data = {
+        email,
+        password
+    }
+    axios
+    .post("http://localhost:8080/login", data)
+    .then((response) => {
+      const accessToken = response.headers['authorization'];
+      const refreshToken = response.headers['refresh_Token'];
+      setCookies('AccessToken',accessToken)
+      setCookies('RefreshToken', refreshToken); 
+    })
+    .catch((error) => {
+      console.error('로그인 에러 : ' + error);
+      alert('로그인에 실패했습니다.');
+    });
+}
+  return (
+    <Card sx={{minWidth:275,maxWidth:"50vw"}}>
+        <CardContent>
+            <Box>
+                <TextField fullWidth label="Email" type="email" variant="standard" onChange={(e)=>{setEmail(e.target.value)}}/>
+                <TextField fullWidth label="Email" type="password" variant="standard" onChange={(e)=>{setPassword(e.target.value)}}/>
+            </Box>
+        </CardContent>
+        <CardActions>
+        <Button fullWidth onClick={() => signInhandler()} variant="contained">로그인</Button>
+        </CardActions>
+    </Card>
+  )
+}
--- src/views/Athentication/index.tsx
+++ src/views/Athentication/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Signup from './SignUp'
+import SignIn from './SignIn'
 
 export default function Athentication() {
   return (
+    <>
     <Signup/>
+    <SignIn/>
+    </>
   )
 }
